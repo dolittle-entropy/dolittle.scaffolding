ee733e2e55033fb6cea1e8bf49d541ecbf57ce97
digitaldias <pedro@digitaldias.com>
2022-05-24 06:35:16 +0200

    Move Configuration models to Models folder

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -126,6 +126,3 @@ namespace DolittleScaffolding.Controllers
     }
 }
 
-public record KafkaConfiguration(Kafka Kafka);
-public record Kafka(string GroupId, string BrokerUrl, string InputTopic, string CommandTopic, string ReceiptsTopic, string ChangeEventsTopic, Ssl Ssl);
-public record Ssl(string Authority, string Certificate, string Key);
---
+++ Models/KafkaConfiguration.cs
@@ -0,0 +1,3 @@
+﻿public record KafkaConfiguration(Kafka Kafka);
+public record Kafka(string GroupId, string BrokerUrl, string InputTopic, string CommandTopic, string ReceiptsTopic, string ChangeEventsTopic, Ssl Ssl);
+public record Ssl(string Authority, string Certificate, string Key);
